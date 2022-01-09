0e0af6f9c7f6c3cc2ccc05b01127bef1e7044a87
RafnunNusrat <44598826+RafnunNusrat@users.noreply.github.com>
2022-01-09 21:47:55 +0600

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::post('logout',[UserController::class,'logout'])->namespace('logout');
 Route::get('/', function () {
-    return view('welcome');
+    return view('logout');
 });
