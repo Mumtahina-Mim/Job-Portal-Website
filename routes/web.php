dc4381f3986f091f81e22e7a68fdf8af07d8149b
akramkhan153 <md.ak153@gmail.com>
2022-01-29 18:39:03 +0600

    data

--- routes/web.php
+++ routes/web.php
@@ -28,13 +28,50 @@ Route::middleware('auth')->prefix('account')->group(function () {
   Route::get('change-password', [AccountController::class, 'changePasswordView'])->name('account.changePassword');
   Route::delete('delete', [AccountController::class, 'deleteAccount'])->name('account.delete');
   Route::put('change-password', [AccountController::class, 'changePassword'])->name('account.changePassword');
+  //savedJobs
+  Route::get('my-saved-jobs', [savedJobController::class, 'index'])->name('savedJob.index');
+  Route::get('my-saved-jobs/{id}', [savedJobController::class, 'store'])->name('savedJob.store');
+  Route::delete('my-saved-jobs/{id}', [savedJobController::class, 'destroy'])->name('savedJob.destroy');
+  //applyjobs
+  Route::get('apply-job', [AccountController::class, 'applyJobView'])->name('account.applyJob');
+  Route::post('apply-job', [AccountController::class, 'applyJob'])->name('account.applyJob');
 
-  //Company part
+  //Admin Role Routes
+  Route::group(['middleware' => ['role:admin']], function () {
+    Route::get('dashboard', [AdminController::class, 'dashboard'])->name('account.dashboard');
+    Route::get('view-all-users', [AdminController::class, 'viewAllUsers'])->name('account.viewAllUsers');
+    Route::delete('view-all-users', [AdminController::class, 'destroyUser'])->name('account.destroyUser');
+
+    Route::get('category/{category}/edit', [CompanyCategoryController::class, 'edit'])->name('category.edit');
+    Route::post('category', [CompanyCategoryController::class, 'store'])->name('category.store');
+    Route::put('category/{id}', [CompanyCategoryController::class, 'update'])->name('category.update');
+    Route::delete('category/{id}', [CompanyCategoryController::class, 'destroy'])->name('category.destroy');
+  });
+
+  //Author Role Routes
+  Route::group(['middleware' => ['role:author']], function () {
+    Route::get('author-section', [AuthorController::class, 'authorSection'])->name('account.authorSection');
+
+    Route::get('job-application/{id}', [JobApplicationController::class, 'show'])->name('jobApplication.show');
+    Route::delete('job-application', [JobApplicationController::class, 'destroy'])->name('jobApplication.destroy');
+    Route::get('job-application', [JobApplicationController::class, 'index'])->name('jobApplication.index');
+
+    Route::get('post/create', [PostController::class, 'create'])->name('post.create');
+    Route::post('post', [PostController::class, 'store'])->name('post.store');
+    Route::get('post/{post}/edit', [PostController::class, 'edit'])->name('post.edit');
+    Route::put('post/{post}', [PostController::class, 'update'])->name('post.update');
+    Route::delete('post/{post}', [PostController::class, 'destroy'])->name('post.destroy');
 
     Route::get('company/create', [CompanyController::class, 'create'])->name('company.create');
     Route::put('company/{id}', [CompanyController::class, 'update'])->name('company.update');
     Route::post('company', [CompanyController::class, 'store'])->name('company.store');
     Route::get('company/edit', [CompanyController::class, 'edit'])->name('company.edit');
     Route::delete('company', [CompanyController::class, 'destroy'])->name('company.destroy');
-  
+  });
+
+  //User Role routes
+  Route::group(['middleware' => ['role:user']], function () {
+    Route::get('become-employer', [AccountController::class, 'becomeEmployerView'])->name('account.becomeEmployer');
+    Route::post('become-employer', [AccountController::class, 'becomeEmployer'])->name('account.becomeEmployer');
+  });
 });
