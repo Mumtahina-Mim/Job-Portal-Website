bdbcce804be7cb4cf13a6ee514046d0428aa5228
mohidulislam-stu2017 <80029999+mohidulislam-stu2017@users.noreply.github.com>
2022-01-30 23:50:22 +0600

    updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,51 +1,24 @@
 <?php
 
+namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
-use App\Category;
-use App\User;
-use App\Role;
+
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
-     * @brief
-     * 
+     *
      * @return void
-     * 
      */
     public function run()
     {
-        Category::truncate();
-    	factory('App\User',20)->create();
-    	factory('App\Company',20)->create();
-    	factory('App\Job',20)->create();
-
-        $categories = [
-
-            'Technology',
-            'Engineering',
-            'Government',
-            'Medical',
-            'Construction',
-            'Software'
-
-        ];
-        foreach($categories as $category){
-            Category::create(['name'=>$category]);
-        }
-
-        Role::truncate();
-        $adminRole = Role::create(['name'=>'admin']);
-
-        $admin = User::create([
-            'name'=>'admin',
-            'email'=>'admin@gmail.com',
-            'password'=>bcrypt('password123'),
-            'email_verified_at'=>NOW()
+        $this->call([
+            CategorySeeder::class,
+            RolePermissionSeeder::class,
+            UserSeeder::class,
+            CompanySeeder::class
         ]);
-
-        $admin->roles()->attach($adminRole);
-
-
-       }
+        // \App\Models\User::factory(10)->create();
+    }
 }
