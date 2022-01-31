aa4de439eea02fdeac6eb5873f8aaf11d080171b
Mumtahina-Mim <m.tahina0911@gmail.com>
2022-01-31 15:17:47 +0600

    saved job unit testing

--- tests/Unit/UnitUserTest.php
+++ tests/Unit/UnitUserTest.php
@@ -17,39 +17,57 @@ class UnitUserTest extends TestCase
         $response->assertStatus(200);
     }
 
-    public function test_post_post_show()
+    public function test_post_show()
     {
         $response = $this->get('/job/{job}');
         $response->assertStatus(404);
     }
 
-    public function test_post_post_create()
+    public function test_post_create()
     {
         $response = $this->get('/post/create');
         $response->assertStatus(404);
     }
    
-    public function test_post_post_store()
+    public function test_post_store()
     {
         $response = $this->post('/post');
         $response->assertStatus(404);
     }
 
-    public function test_post_post_edit()
+    public function test_post_edit()
     {
         $response = $this->get('/post/{post}/edit');
         $response->assertStatus(404);
     }
 
-    public function test_post_post_update()
+    public function test_post_update()
     {
         $response = $this->put('post/{post}');
         $response->assertStatus(404);
     }
     
-    public function test_post_post_delete()
+    public function test_post_delete()
     {
         $response = $this->delete('post/{post}');
+        $response->assertStatus(404);
+    }
+
+    public function test_saved_job()
+    {
+        $response = $this->get('my-saved-jobs');
+        $response->assertStatus(404);
+    }
+
+    public function test_saved_job_store()
+    {
+        $response = $this->get('my-saved-jobs/{id}');
+        $response->assertStatus(404);
+    }
+
+    public function test_saved_job_delete()
+    {
+        $response = $this->delete('my-saved-jobs/{id}');
         $response->assertStatus(404);
     }
 }
